263baa0ce8d214d1646654aa82d1081c4996d34a
Susannah Sun <thisis.susannahsun@gmail.com>
2025-03-13 16:41:23 -0700

    use caching to load data upon opening app

--- src/components/callbacks.py
+++ src/components/callbacks.py
@@ -1,12 +1,11 @@
 from dash.dependencies import Input, Output
 from dash import html, dcc
-
 from components.charts import line_chart, radar_chart, map_viz
-from components.data import happiness_data, all_features, all_countries, geo_countries
-
+from components.data import happiness_data, radar_data, all_features, all_countries, geo_countries, load_happiness_data, filter_happiness_data
 import pandas as pd
 import altair as alt
 alt.data_transformers.enable('vegafusion')
+
 
 def register_callbacks(app):
    # No more than 5 categories can be selected
@@ -46,7 +45,7 @@ def register_callbacks(app):
        Input('year_slider_2', 'value')]
    )
    def update_radar_chart(selected_categories, selected_countries, selected_year):
-        
+    
     # If not 3-5 categories or more than 1 country selected, display message
     if len(selected_categories) < 3 or len(selected_countries) < 2:
         return None, html.Label('Please select 3-5 categories and up to 3 countries to see the radar chart.')
@@ -58,19 +57,15 @@ def register_callbacks(app):
     
     return dcc.Graph(id='radar-chart', figure=radar_chart(filtered_df)), None
    
-   # Creat line chart 
+   # Create line chart 
    @app.callback(
     [Output('line-chart', 'figure')],
     [Input('line-chart-feature-dropdown', 'value'),
     Input('continent-dropdown', 'value'),
     Input('year_slider_1', 'value')])   
    def update_line_chart(selected_feature, selected_continent, selected_year):
-    if selected_continent == 'All Continents':
-       filter_df = happiness_data[(happiness_data["Year"] == selected_year)]
-    else:
-        filter_df = happiness_data[(happiness_data["Continent"] == selected_continent) & (happiness_data["Year"] == selected_year)]
-    fig = line_chart(filter_df, selected_feature, selected_continent)
-
+    filtered_df = filter_happiness_data(selected_year, selected_continent)
+    fig = line_chart(filtered_df, selected_feature, selected_continent)
     return [fig]
    
    # Display map
@@ -81,13 +76,7 @@ def register_callbacks(app):
     Input('year_slider_1', 'value')]
     )
    def map(selected_feature, selected_continent, selected_year):
-    if selected_continent == 'All Continents':
-       filter_df = happiness_data[(happiness_data["Year"] == selected_year)]
-    else:
-        filter_df = happiness_data[(happiness_data["Continent"] == selected_continent) & (happiness_data["Year"] == selected_year)]
-
+    filter_df = filter_happiness_data(selected_year, selected_continent)
     filtered_df = pd.merge(geo_countries, filter_df, on="Country", how="left")
-    
-    return map_viz(geo_countries, filtered_df, selected_feature)
-   
 
+    return map_viz(geo_countries, filtered_df, selected_feature)
--- src/components/data.py
+++ src/components/data.py
@@ -2,17 +2,30 @@ import pandas as pd
 import geopandas as gpd
 from sklearn.preprocessing import MinMaxScaler
 import os
+import functools
 
 file_path = os.path.join(os.path.dirname(__file__), "../../data/processed/World_Happiness_processed_data.csv")
 
 # app data
-happiness_data = pd.read_csv(os.path.abspath(file_path))
+@functools.lru_cache(maxsize=1)
+def load_happiness_data():
+    """Loads and caches the processed dataset"""
+    return pd.read_csv(os.path.abspath(file_path))
 
+happiness_data = load_happiness_data()
 cols = ['Happiness Score', 'GDP per Capita', 'Social Support', 'Healthy Life Expectancy', 'Freedom to Make Life Choices', 'Generosity', 'Perceptions of Corruption']
 all_features = [{'label': feature, 'value': feature} for feature in cols]
 all_countries = [{'label': country, 'value': country} for country in sorted(happiness_data['Country'].unique())]
 all_continents = [{'label': 'All Continents', 'value': 'All Continents'}] + [{'label': continent, 'value': continent} for continent in happiness_data['Continent'].unique()]
 all_years = [{"label": year, "value": year} for year in sorted(happiness_data["Year"].unique())]
+
+@functools.lru_cache()
+def filter_happiness_data(year, continent):
+   """Filter processed data based on year and continent"""
+   df = load_happiness_data()
+   if continent == "All Continents":
+      return df[df["Year"] == year]
+   return df[(df["Continent"] == continent) & (df["Year"] == year)]
 
 # map data 
 file_path_geo = os.path.join(os.path.dirname(__file__), "../../data/processed/world_countries.parquet")
